237c102bd6ad869f6760cf151c0f53c773094a37
Ronald Maciel <ronaldsmaciel2@outlook.com>
2021-04-25 20:38:03 -0300

    Add comments

--- Assignment-1/Part3.py
+++ Assignment-1/Part3.py
@@ -3,20 +3,25 @@ class Stack:
     def __init__(self):
         self.stack = []
 
+    # Add the element to the top of the stack
     def push(self, element):
         self.stack.append(element)
 
+    # Remove the last element of the stack, i.e., the element present in the top of the stack
     def pop(self):
         if len(self.stack) == 0:
             return None
         else:
             self.stack.pop()
 
+    # Return the last element of the stack
     def top(self):
         return self.stack[-1]
 
+    # Returns a boolean that verifies if stack is empty
     def isEmpty(self):
         return self.stack == []
 
+    # Return the actual size of the stack
     def size(self):
         return len(self.stack)
