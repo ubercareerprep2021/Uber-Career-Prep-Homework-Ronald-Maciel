42a9f038f37380ba3b222abadd4432b67cd9ea3e
Ronald Maciel <ronaldsmaciel2@outlook.com>
2021-04-30 21:30:38 -0300

    iterativ reverse

--- Assignment-1/Part5.py
+++ Assignment-1/Part5.py
@@ -1,7 +1,16 @@
 from Part4 import *
 
 def reverseLinkedList_Iteratively(linkedList: LinkedList):
-    print("a")
+    prev_node = None
+    current = linkedList.head
+
+    while (current != None):
+        next_node = current.next
+        current.next = prev_node
+        prev_node = current
+        current = next_node
+    linkedList.head = prev_node
+    return linkedList
 
 
 
