2a04263d380a23a91949e4be255f91e38d56e501
Ronald Maciel <ronaldsmaciel2@outlook.com>
2021-06-23 22:58:54 -0300

    finished insert

--- Assignment-2/Trees/Ex4.py
+++ Assignment-2/Trees/Ex4.py
@@ -1,23 +1,45 @@
 class BinarySearchTree:
-    def __init__(self, root):
-        self.root = root
+    def __init__(self, key, left = None, right = None):
+        self.key = key
+        self.left = left
+        self.right = right
 
+    def insert(self, key):
+        if self.key is None:
+            self.key = key
+            return
+        else:
+            if self.key < key:
+                if self.right is None:
+                    self.right = BinarySearchTree(key)
+                else:   
+                    self.right.insert(key)
+            else:
+                if self.left is None:
+                    self.left = BinarySearchTree(key)
+                else:
+                    self.left.insert(key)
 
-    class Node():
+    
+    def find(self, key):
+        print(key)
+
+    class Node:
         def __init__(self, key, left = None, right = None):
             self.key = key
             self.left = left
             self.right = right
 
-    def insert(self, key):
-        print(key)
-
-    def find(self, key):
-        print(key)
-
-
-
-
 
 if __name__ == "__main__":
-    tree = BinarySearchTree(0)
+    print("a")
+    # root_node = BinarySearchTree.Node(16)
+    tree = BinarySearchTree(16)
+    tree.insert(10)
+    tree.insert(21)
+    tree.insert(7)
+    tree.insert(13)
+    tree.insert(18)
+    tree.insert(29)
+    tree.insert(99)
+    
