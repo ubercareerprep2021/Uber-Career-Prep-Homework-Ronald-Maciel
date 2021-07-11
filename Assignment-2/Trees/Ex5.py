d20ef1bcdab4e62c96f34287501e08940835a018
Ronald Maciel <ronaldsmaciel2@outlook.com>
2021-07-11 17:37:13 -0300

    basic class structure

---
+++ Assignment-2/Trees/Ex5.py
@@ -0,0 +1,18 @@
+class ListPhoneBook:
+    def __init__(self):
+        self.list = list()
+    
+    def size(self) -> int:
+        print("a")
+
+    def insert(self, dict):
+        print("a")
+
+    def find(self, name) -> int:
+        print("a")
+
+
+# class BinarySearchTreePhoneBook:
+
+if __name__ == "__main__":
+    list = ListPhoneBook()
