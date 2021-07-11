13ea070de5722b7cfa15ff7487b65fb82d72f7cc
Ronald Maciel <ronaldsmaciel2@outlook.com>
2021-07-11 18:01:02 -0300

    testing PhoneBook class

--- Assignment-2/Trees/Ex5.py
+++ Assignment-2/Trees/Ex5.py
@@ -3,16 +3,32 @@ class ListPhoneBook:
         self.list = list()
     
     def size(self) -> int:
-        print("a")
+        return len(self.list)
 
-    def insert(self, dict):
-        print("a")
+    def insert(self, name, phoneNumber):
+        self.list.append({"name": name, "phone_number": phoneNumber})
 
     def find(self, name) -> int:
-        print("a")
-
+        for contact in self.list:
+            # print(name)
+            # print(contact.get("number"))
+            if name == contact.get("name"):
+                return contact.get("number")
+        return -1
 
 # class BinarySearchTreePhoneBook:
 
 if __name__ == "__main__":
-    list = ListPhoneBook()
+    list_phoneBook = ListPhoneBook()
+    list_size = list_phoneBook.size()
+    print(list_size)
+
+    list_phoneBook.insert("ABC", 1111111111)
+    list_phoneBook.insert("XYZ", 9999999999)
+    list_phoneBook.insert("DEF", 2222222222)
+
+    list_size = list_phoneBook.size()
+    print(list_size)
+
+    phone = list_phoneBook.find("ABC")
+    print(phone)
