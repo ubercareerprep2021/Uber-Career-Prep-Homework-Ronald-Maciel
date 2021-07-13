b90084412d8c47f91cf2210510d4282b3ca6d3af
Ronald Maciel <ronaldsmaciel2@outlook.com>
2021-07-12 22:52:40 -0300

    start graph ex1

---
+++ Assignment-2/Graphs/Ex1.py
@@ -0,0 +1,19 @@
+class GraphNode:
+    def __init__(self, data):
+        self.data = data
+
+class GraphWithAdjacencyList:
+    def __init__(self):
+        pass
+        
+    def addNode(self, key):
+        pass
+
+    def removeNode(self, key):
+        pass
+    
+    def addEdge(self, node1, node2):
+        pass
+
+    def removeEdge(self, node1, node2):
+        pass
